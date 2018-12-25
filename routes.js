72c92d0c1464efeb9f48f6990bbbf7bfcf89a4aa
MarioSalinas <mario_1605@live.com.mx>
2018-12-25 16:10:18 -0600

    State with cookies

--- routes.js
+++ routes.js
@@ -33,6 +33,11 @@ module.exports = [
     handler: site.login
   },
   {
+    method: 'GET',
+    path: '/logout',
+    handler: user.logout
+  },
+  {
     method: 'POST',
     path: '/validate-user',
     options:{
