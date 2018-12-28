62a63b292de7786a3a8e8f7451c1f0fc0c9ee85b
MarioSalinas <mario_1605@live.com.mx>
2018-12-27 22:20:32 -0600

    Monitoring server with good pluging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const Hapi = require('hapi')
 const handlerbars = require('./lib/helpers')
 const inert = require('inert')
+const good = require('good')
 const methods = require('./lib/methods')
 const path = require('path')
 const routes = require('./routes')
@@ -23,6 +24,19 @@ async function init () {
   try {
     await server.register(inert)
     await server.register(vision)
+    await server.register({
+      plugin: good,
+      options: {
+        reporters: {
+          console: [
+            {
+              module: 'good-console'
+            },
+            'stdout'
+          ]
+        }
+      }
+    })
 
     server.method('setAnswerRight', methods.setAnswerRight)
     server.method('getLast', methods.getLast, {
@@ -57,15 +71,15 @@ async function init () {
     process.exit(1)
   }
 
-  console.log(`Servidor lanzado en: ${server.info.uri}`)
+  server.log('info', `Servidor lanzado en: ${server.info.uri}`)
 }
 
 process.on('unhandledRejection', error => {
-  console.error('UnhandledRejection', error.message, error)
+  server.log('UnhandledRejection', error)
 })
 
 process.on('unhandledException', error => {
-  console.error('unhandledException', error.message, error)
+  server.log('unhandledException', error)
 })
 
 init()
